ea49e3e3bacecb45113c40f67738bb4216c0258e
jose martinez <jose.a.m089@gmail.com>
2019-03-18 13:10:13 -0500

    Init

---
+++ Dice Game.js
@@ -0,0 +1,9 @@
+// (15 points): As a developer, I want to come up with a game concept played with dice,
+// ensuring that my game concept is more complicated than “War”.
+// (15 points): As a developer, I want my game to have gameplay functionality.
+// (10 points): As a developer, I want to have one function capable of “rolling a die” (by generating a random number),
+// regardless of the number of sides.
+// (10 points): As a developer, I want to utilize six different dice within my game.
+// (Recommended dice are 4-sided, 6-sided, 8-sided, 10-sided, 12-sided, and 20-sided.Different dice may be substituted.
+// No 2-sided die.)
+// (5 points): As a developer, I want to make consistent commits accompanied with good, descriptive commit messages.
