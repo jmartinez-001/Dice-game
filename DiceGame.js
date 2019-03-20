d7688b999d68835c3db3cae23c26f0fc1122b1ab
jose martinez <jose.a.m089@gmail.com>
2019-03-19 22:13:29 -0500

    gameplay functionality working

--- DiceGame.js
+++ DiceGame.js
@@ -1,11 +1,16 @@
 // (15 points): As a developer, I want to come up with a game concept played with dice,
 // ensuring that my game concept is more complicated than “War”.
+
 // (15 points): As a developer, I want my game to have gameplay functionality.
+
 // (10 points): As a developer, I want to have one function capable of “rolling a die” (by generating a random number),
 // regardless of the number of sides.
+
 // (10 points): As a developer, I want to utilize six different dice within my game.
+
 // (Recommended dice are 4-sided, 6-sided, 8-sided, 10-sided, 12-sided, and 20-sided.Different dice may be substituted.
 // No 2-sided die.)
+
 // (5 points): As a developer, I want to make consistent commits accompanied with good, descriptive commit messages.
 
 
@@ -16,16 +21,40 @@
 // let d12 = document.getElementById('dice5');
 // let d20 = document.getElementById('dice6');
 
+let dice1 = document.getElementById('dice1');
+let dice2 = document.getElementById('dice2');
+let dice3 = document.getElementById('dice3');
+let dice4 = document.getElementById('dice4');
+let dice5 = document.getElementById('dice5');
+let dice6 = document.getElementById('dice6');
+let player1score = document.getElementById('player1score');
+let player1total = document.getElementById('player1total');
+let player1bank = document.getElementById('player1bank');
+
+function startGame() {
+
+  dice1.innerHTML = 0;
+  dice2.innerHTML = 0;
+  dice3.innerHTML = 0;
+  dice4.innerHTML = 0;
+  dice5.innerHTML = 0;
+  dice6.innerHTML = 0;
+  player1score.innerHTML = 0;
+  player1total.innerHTML = 0;
+  player1bank.innerHTML = 0;
+}
+
 let d4s;
 let d6s;
 let d8s;
 let d10s;
 let d12s;
 let d20s;
+let totalScore = 0;
+let totalTurn = 0;
+let runningScore= 0;
 
-
-
-function roll(){
+function roll() {
 
   d4s = rollDice(4);
   d6s = rollDice(6);
@@ -42,63 +71,56 @@ function roll(){
   dice6.innerHTML = d20s;
 
   let totalScore = gameScoring();
-  let player1bank = document.getElementById('player1bank');
+  // let player1bank = document.getElementById('player1bank');
 
 }
 
-function rollDice(n){
+function rollDice(n) {
   let randomNumber = Math.floor(Math.random() * n + 1);
   return randomNumber;
 }
 
-let totalScore;
-let totalTurn;
-let runningScore;
-let player1score = document.getElementById('player1score');
-let player1total = document.getElementById('player1total');
-
-
 function gameScoring() {
-  let totalScore = 0;
+  totalScore;
   let totalTurn = player1score.innerHTML;
   let sum = d4s + d6s + d8s + d10s + d12s + d20s;
-  let scoreoutput = sum;
+  scoreoutput = sum;
   let scorearray = [d4s, d6s, d8s, d10s, d12s, d20s];
   let orderedarray = scorearray.sort(function(a, b) {
-    return a - b});
+    return a - b
+  });
   let scorelength = orderedarray.length;
   // orderedarray[0] = 1;
   // orderedarray[1] = 1;
   if (orderedarray[0] == 1 && orderedarray[1] == 1) {
-      scoreoutput = 0;
-      runningScore = 0;
-      totalScore = 0;
-} if ((orderedarray[scorelength]) == 20) {
     scoreoutput = 0;
     runningScore = 0;
+    totalScore = 0;
   }
-  else {
+  if ((orderedarray[scorelength - 1]) == 20) {
+    scoreoutput = 0;
+    runningScore = 0;
+  } else {
     runningScore = turnScore(scoreoutput);
   }
 
   player1score.innerHTML = scoreoutput;
   player1total.innerHTML = runningScore;
-  return totalScore;
+  player1bank.innerHTML = totalScore;
+  return;
 }
 
-
-
-function bankScore(){
-
-  totalScore = ((parseInt(player1bank.innerHTML)) +runningScore);
-  player1bank.innerHTML = totalScore;
-  player1score.innerHTML = 0;
-  player1total.innerHTML = 0;
-
+function bankScore() {
+  // if (scoreoutput != 0 && runningScore != 0) {
+    totalScore = totalScore + runningScore;
+    player1bank.innerHTML = totalScore;
+    player1score.innerHTML = 0;
+    player1total.innerHTML = 0;
+  // }
   return;
 }
 
 function turnScore(scoreoutput) {
-  let score = ((parseInt(player1score.innerHTML)) + scoreoutput);
+  let score = ((parseInt(player1total.innerHTML)) + scoreoutput);
   return score;
 }
